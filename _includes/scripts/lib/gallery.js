4527c16ec416a23fe93d0cfd9ba394f959f4f561
Chaf Chen <35370033+Chaphlagical@users.noreply.github.com>
2020-05-27 12:02:19 +0800

    update

--- _includes/scripts/lib/gallery.js
+++ _includes/scripts/lib/gallery.js
@@ -9,27 +9,41 @@
         '<div class="swiper__button swiper__button--prev fas fa-chevron-left"></div>' +
         '<div class="swiper__button swiper__button--next fas fa-chevron-right"></div>' +
       '</div>';
-    function Gallery(root, items, options) {
+    function setState($item, zoom, translate) {
+      $item.css('transform', 'scale(' + zoom + ') translate(' + translate.x +  'px,' + translate.y + 'px)');
+    }
+    function Gallery(root, items) {
       this.$root = $(root);
       this.$swiper = null;
-      this.$itemWrapper = null;
-      this.swiper = null;
+      this.$swiperWrapper = null;
+      this.$activeItem = null;
       this.$items = [];
-      this.items = items;
-      this.disabled = false;
       this.contentWidth = 0;
       this.contentHeight = 0;
+      this.swiper = null;
+      this.items = items;
+      this.disabled = false;
+      this.curIndex = 0;
+      this.touchCenter = null;
+      this.lastTouchCenter = null;
+      this.zoomRect = null;
+      this.lastZoomRect = null;
+      this.lastTranslate = null;
+      this.translate = null;
+      this.lastZoom = 1;
+      this.preZoom = 1;
+      this.zoom = 1;
     }
     Gallery.prototype.init = function() {
-      var i, item, items = this.items, itemsSnippet = '', size, self = this;
+      var i, item, items = this.items, size, self = this, touchstartFingerCount = 0;
       this.$root.append(template);
       this.$swiper = this.$root.find('.gallery__swiper');
-      this.$itemWrapper = this.$root.find('.swiper__wrapper');
-      this.contentWidth = this.$itemWrapper && this.$itemWrapper.width();
-      this.contentHeight = this.$itemWrapper && this.$itemWrapper.height();
+      this.$swiperWrapper = this.$root.find('.swiper__wrapper');
+      this.contentWidth = this.$swiperWrapper && this.$swiperWrapper.width();
+      this.contentHeight = this.$swiperWrapper && this.$swiperWrapper.height();
       for (i = 0; i < items.length; i++) {
         item = items[i];
-        size = this.calculateImageSize(item.w, item.h);
+        size = this._calculateImageSize(item.w, item.h);
         this.$items.push($(
           '<div class="swiper__slide">' +
             '<div class="gallery-item">' +
@@ -40,11 +54,15 @@
           '</div>'
         ));
       }
-      this.$itemWrapper && this.$itemWrapper.append(this.$items);
-      this.swiper = this.$swiper && this.$swiper.swiper();
+      this.$swiperWrapper && this.$swiperWrapper.append(this.$items);
+      this.swiper = this.$swiper && this.$swiper.swiper({
+        onChangeEnd: function() {
+          self._handleChangeEnd.apply(self, Array.prototype.slice.call(arguments));
+        }
+      });
       $(window).on('resize', function() {
         if (self.disabled) { return; }
-        self.resizeImageSize();
+        self._resizeImageSize();
       });
       // Char Code: 37  ⬅, 39  ➡
       $(window).on('keyup', function(e) {
@@ -55,8 +73,82 @@
           self.swiper && self.swiper.next();
         }
       });
+      function getRect(touch0, touch1) {
+        return {
+          o: {
+            x: (touch0.pageX + touch1.pageX) / 2,
+            y: (touch0.pageY + touch1.pageY) / 2
+          },
+          w: Math.abs(touch0.pageX - touch1.pageX),
+          h: Math.abs(touch0.pageY - touch1.pageY)
+        };
+      }
+      function getTouches(e) {
+        return e.touches || e;
+      }
+      function getTouchesCount(e) {
+        if (e.touches) {
+          return e.touches.length;
+        } else {
+          return 1;
+        }
+      }
+      this.$swiperWrapper.on('touchstart', function(e) {
+        var touch0, touch1, rect;
+        touchstartFingerCount = getTouchesCount(e);
+        if (touchstartFingerCount > 1) {
+          touch0 = e.touches[0];
+          touch1 = e.touches[1];
+          rect = getRect(touch0, touch1);
+          self.lastZoomRect = { w: rect.w, h: rect.h };
+          self.lastTouchCenter = rect.o;
+        } else {
+          var touch = getTouches(e)[0];
+          self.lastTouchCenter = { x: touch.pageX, y: touch.pageY };
+        }
+      });
+      this.$swiperWrapper.on('touchmove', function(e) {
+        if (touchstartFingerCount === getTouchesCount(e)) {
+          if (touchstartFingerCount > 1) {
+            var touch0 = e.touches[0];
+            var touch1 = e.touches[1];
+            var rect = getRect(touch0, touch1);
+            self.zoomRect = { w: rect.w, h: rect.h };
+            self.touchCenter = rect.o;
+            self._zoom(); self._translate();
+            setState(self.$activeItem, self.zoom, self.translate);
+          } else {
+            var touch = getTouches(e)[0];
+            self.touchCenter = { x: touch.pageX, y: touch.pageY };
+            self._translate();
+            setState(self.$activeItem, self.zoom, self.translate);
+          }
+        }
+      });
+      this.$swiperWrapper.on('touchend', function(e) {
+        self.lastZoom = self.zoom;
+        self.lastTranslate = self.translate;
+        touchstartFingerCount = 0;
+      });
+      this.$root.on('touchmove', function(e) {
+        if (self.disabled) { return; }
+        e.preventDefault();
+      });
     };
-    Gallery.prototype.calculateImageSize = function(w, h) {
+
+    Gallery.prototype._translate = function() {
+      this.translate = this.touchCenter && this.lastTouchCenter && this.lastTranslate ? {
+        x: (this.touchCenter.x - this.lastTouchCenter.x) / this.zoom + this.lastTranslate.x,
+        y: (this.touchCenter.y - this.lastTouchCenter.y) / this.zoom + this.lastTranslate.y
+      } : { x: 0, y: 0 };
+    }
+    Gallery.prototype._zoom = function() {
+      this.zoom = (this.zoomRect.w + this.zoomRect.h) / (this.lastZoomRect.w + this.lastZoomRect.h) * this.lastZoom;
+      this.zoom > 1 ? this.$activeItem.addClass('zoom') : this.$activeItem.removeClass('zoom');
+      this.preZoom = this.zoom;
+    }
+
+    Gallery.prototype._calculateImageSize = function(w, h) {
       var scale = 1;
       if (this.contentWidth > 0 && this.contentHeight > 0 && w > 0 && h > 0) {
         scale = Math.min(
@@ -65,20 +157,31 @@
       }
       return { w: Math.floor(w * scale), h: Math.floor(h * scale) };
     };
-    Gallery.prototype.resizeImageSize = function() {
+
+    Gallery.prototype._resizeImageSize = function() {
       var i, $item, $items = this.$items, item, size;
-      this.contentWidth = this.$itemWrapper && this.$itemWrapper.width();
-      this.contentHeight = this.$itemWrapper && this.$itemWrapper.height();
+      this.contentWidth = this.$swiperWrapper && this.$swiperWrapper.width();
+      this.contentHeight = this.$swiperWrapper && this.$swiperWrapper.height();
       if ($items.length < 1) { return; }
       for (i = 0; i < $items.length; i++) {
         item = this.items[i], $item = $items[i];
-        size = this.calculateImageSize(item.w, item.h);
+        size = this._calculateImageSize(item.w, item.h);
+        item.width = size.w; item.height = size.h;
         $item && $item.find('img').css({ width: size.w, height: size.h });
       }
-    }
+    };
+    Gallery.prototype._handleChangeEnd = function(index, $dom, preIndex, $preDom) {
+      this.curIndex = index;
+      this.lastZoomRect = null; this.lastZoomRect = null;
+      this.lastTranslate = this.translate = { x: 0, y:0 };
+      this.lastZoom = this.preZoom = this.zoom = 1;
+      this.$activeItem = $dom.find('.gallery-item__content');
+      setState($preDom.find('.gallery-item__content'), this.zoom, this.translate);
+    };
+
     Gallery.prototype.refresh = function() {
       this.swiper && this.swiper.refresh();
-      this.resizeImageSize();
+      this._resizeImageSize();
     };
     Gallery.prototype.setOptions = function(options) {
       this.disabled = options.disabled;
--- _includes/search-providers/default/search.js
+++ _includes/search-providers/default/search.js
@@ -1,7 +1,8 @@
 var SOURCES = window.TEXT_VARIABLES.sources;
 var PAHTS = window.TEXT_VARIABLES.paths;
 window.Lazyload.js([SOURCES.jquery, PAHTS.search_js], function() {
-  var searchData = window.TEXT_SEARCH_DATA ? initData(window.TEXT_SEARCH_DATA) : {};
+  var search = (window.search || (window.search = {}));
+  var searchData = window.TEXT_SEARCH_DATA || {};
 
   function memorize(f) {
     var cache = {};
@@ -10,21 +11,6 @@ window.Lazyload.js([SOURCES.jquery, PAHTS.search_js], function() {
       if (key in cache) return cache[key];
       else return cache[key] = f.apply(this, arguments);
     };
-  }
-
-  function initData(data) {
-    var _data = [], i, j, key, keys, cur;
-    keys = Object.keys(data);
-    for (i = 0; i < keys.length; i++) {
-      key = keys[i], _data[key] = [];
-      for (j = 0; j < data[key].length; j++) {
-        cur = data[key][j];
-        cur.title = window.decodeUrl(cur.title);
-        cur.url = window.decodeUrl(cur.url);
-        _data[key].push(cur);
-      }
-    }
-    return _data;
   }
 
   /// search
@@ -71,60 +57,21 @@ window.Lazyload.js([SOURCES.jquery, PAHTS.search_js], function() {
   }
 
   // search box
-  var $searchBox = $('.js-search-box');
-  var $searchInput = $searchBox.children('input');
-  var $searchClear = $searchBox.children('.js-icon-clear');
   var $result = $('.js-search-result'), $resultItems;
   var lastActiveIndex, activeIndex;
 
-  function searchBoxEmpty() {
-    $searchBox.removeClass('not-empty'); $result.html(null);
+  function clear() {
+    $result.html(null);
     $resultItems = $('.search-result__item'); activeIndex = 0;
   }
-
-  $searchInput.on('input', window.throttle(function() {
-    var val = $(this).val();
-    if (val === '' || typeof val !== 'string') {
-      searchBoxEmpty();
-    } else {
-      $searchBox.addClass('not-empty'); $result.html(render(searchByQuery(val)));
-      $resultItems = $('.search-result__item'); activeIndex = 0;
-      $resultItems.eq(0).addClass('active');
-    }
-  }, 400));
-  $searchInput.on('focus', function() {
-    $(this).addClass('focus');
-  });
-  $searchInput.on('blur', function() {
-    $(this).removeClass('focus');
-  });
-  $searchClear.on('click', function() {
-    $searchInput.val(''); searchBoxEmpty();
-  });
-
-  // search panel
-  var $searchModal = $('.js-page-search-modal');
-  var $searchToggle = $('.js-search-toggle');
-  var modalVisible = false;
-
-  var searchModal = $searchModal.modal({ onChange: handleModalChange });
-
-  function handleModalChange(visible) {
-    modalVisible = visible;
-    if (visible) {
-      $searchInput[0].focus();
-    } else {
-      $searchInput[0].blur();
-      setTimeout(function() {
-        $searchInput.val(''); searchBoxEmpty();
-        window.pageAsideAffix && window.pageAsideAffix.refresh();
-      }, 400);
-    }
+  function onInputNotEmpty(val) {
+    $result.html(render(searchByQuery(val)));
+    $resultItems = $('.search-result__item'); activeIndex = 0;
+    $resultItems.eq(0).addClass('active');
   }
 
-  $searchToggle.on('click', function() {
-    modalVisible ? searchModal.hide() : searchModal.show();
-  });
+  search.clear = clear;
+  search.onInputNotEmpty = onInputNotEmpty;
 
   function updateResultItems() {
     lastActiveIndex >= 0 && $resultItems.eq(lastActiveIndex).removeClass('active');
@@ -144,8 +91,9 @@ window.Lazyload.js([SOURCES.jquery, PAHTS.search_js], function() {
     }
   }
 
-  // Char Code: 13  Enter, 37  ⬅, 38  ⬆, 39  ➡, 40  ⬇, 83  S, 191 /
+  // Char Code: 13  Enter, 37  ⬅, 38  ⬆, 39  ➡, 40  ⬇
   $(window).on('keyup', function(e) {
+    var modalVisible = search.getModalVisible && search.getModalVisible();
     if (modalVisible) {
       if (e.which === 38) {
         modalVisible && moveActiveIndex('up');
@@ -153,10 +101,6 @@ window.Lazyload.js([SOURCES.jquery, PAHTS.search_js], function() {
         modalVisible && moveActiveIndex('down');
       } else if (e.which === 13) {
         modalVisible && $resultItems && activeIndex >= 0 && $resultItems.eq(activeIndex).children('a')[0].click();
-      }
-    } else {
-      if (!window.isFormElement(e.target || e.srcElement) && (e.which === 83 || e.which === 191)) {
-        modalVisible || searchModal.show();
       }
     }
   });
