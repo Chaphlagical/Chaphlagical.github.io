deb4f2ccab94a3eb6bc2170f85325b63b90ce00c
Chaf Chen <35370033+Chaphlagical@users.noreply.github.com>
2020-09-27 21:54:58 +0800

    update

--- assets/js/particles/app.js
+++ assets/js/particles/app.js
@@ -8,7 +8,7 @@ particlesJS('particles-js',
         "value": 50,
         "density": {
           "enable": true,
-          "value_area": 800
+          "value_area": 100
         }
       },
       "color": {
